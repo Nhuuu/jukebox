c5154cb4d6ddfb64f6aaac74e8c705d9ee5d45c2
idothestamping <douglas.klemp@gmail.com>
2019-02-25 15:12:06 -0800

    alert auto remove

---
+++ public/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  // alert window auto disappear
+  $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#success-alert").slideUp('close');
+  });
+  // materialize side menu on mobile
+  $('.sidenav').sidenav();
+  // materialize carousel
+});
