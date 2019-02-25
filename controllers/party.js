d22e133928391ba4d61f4e178498a35d4e944625
hayeun7777 <kellyhypark@gmail.com>
2019-02-25 15:16:10 -0800

    commit

--- controllers/party.js
+++ controllers/party.js
@@ -5,7 +5,7 @@ var db = require('../models');
 
 // Need post route for create party
 router.get('/', (req, res) => {
-  res.send("party is here!");
+  res.render('parties/show');
 })
 
 
@@ -17,7 +17,9 @@ router.get('/token', (req, res) => {
 
 
 // Get main jukebox page, tie to join a party/nav for jukebox
-router.get('/jukebox')
+router.get('/jukebox', (req, res) => {
+  res.send('jukebox main page')
+})
 
 
 
