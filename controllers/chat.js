f7cda13c11af2bbfec64fe15e76513544d54f718
idothestamping <douglas.klemp@gmail.com>
2019-02-26 15:43:40 -0800

    merge to upstream

--- controllers/chat.js
+++ controllers/chat.js
@@ -6,21 +6,21 @@ router.get('/', (req, res) => {
   res.render('parties/chat');
 })
 
-io.sockets.on('connection', function(socket) {
-  socket.on('username', function(username) {
-      socket.username = req.user.username;
-      io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-  });
+// io.sockets.on('connection', function(socket) {
+//   socket.on('username', function(username) {
+//       socket.username = req.user.username;
+//       io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+//   });
 
-  socket.on('disconnect', function(username) {
-      io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-  })
+//   socket.on('disconnect', function(username) {
+//       io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+//   })
 
-  socket.on('chat_message', function(message) {
-      io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-  });
+//   socket.on('chat_message', function(message) {
+//       io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+//   });
 
-});
+// });
 
 
 module.exports = router;
